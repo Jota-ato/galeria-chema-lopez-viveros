e276c1d35f4c3838fb2afdf8908c1a46ceb7037a
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-07-09 18:50:52 -0600

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+export const metadata: Metadata = {
+  title: "Galeria Chema López Viveros",
+  description: "Galería oficial de Chema López Viveros, artista visual y fotógrafo.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("h-full antialiased", "font-sans", geist.variable)}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
