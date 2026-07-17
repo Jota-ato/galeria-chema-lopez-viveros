cc6043103df266cd0307ebeb9850b8e0d037dbd2
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-07-17 11:52:13 -0600

    fix: update header background style for improved visibility

--- src/shared/components/public/header.tsx
+++ src/shared/components/public/header.tsx
@@ -4,7 +4,7 @@ import { Container } from "../layout/container";
 
 export function Header() {
   return (
-    <div className="w-full fixed top-0 left-0 z-50 bg-linear-to-b from-black/40 to-black/5 h-16 flex items-center">
+    <div className="w-full fixed top-0 left-0 z-50 bg-black/20 backdrop-blur-xs h-16 flex items-center">
       <Container className="flex items-center justify-between text-slate-50">
         <Link href="/">Chema López Viveros</Link>
         <Navigation />
