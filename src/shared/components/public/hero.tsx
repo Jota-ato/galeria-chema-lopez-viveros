d55b5ecb0e58721aa2a73dda5ff9fcfe906aa517
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-08-12 11:08:06 -0600

    feat: add midu tailwind animations

--- src/shared/components/public/hero.tsx
+++ src/shared/components/public/hero.tsx
@@ -1,38 +1,25 @@
 import { Heading } from "@/shared/components/typography/heading";
 import { Button } from "@/shared/components/ui/button";
+import { ChevronDown } from "lucide-react";
 import Link from "next/link";
 
 export function Hero() {
   return (
     <section className="bg-[url('/img/hero.jpeg')] bg-center bg-no-repeat bg-cover min-h-screen">
-      <div className="bg-black/40 size-full min-h-screen flex flex-col gap-8 items-center justify-end text-white p-8 md:pb-12">
-        <Heading className="sm:text-5xl! max-w-xs sm:max-w-xl">
+      <div className="relative inset-0 bg-black/40 size-full min-h-screen flex flex-col gap-8 items-center justify-center text-white p-8 md:pb-12">
+        <Heading className="text-4xl sm:text-6xl! max-w-xs md:max-w-2xl">
           Pintura contemporánea
         </Heading>
-        <div className="flex gap-4 flex-col md:flex-row w-full md:w-auto">
-          <Button
-            size="lg"
-            render={
-              <a
-                href={`https://wa.me/${process.env.ARTIST_PHONE}`}
-                target="_blank"
-              />
-            }
-            nativeButton={false}
-            className="cursor-pointer text-base md:text-xl font-bold px-8 py-6"
-          >
-            Contáctame
-          </Button>
-          <Button
-            variant="secondary"
-            size="lg"
-            render={<Link href="/obras" />}
-            nativeButton={false}
-            className="cursor-pointer text-base md:text-xl font-bold px-8 py-6"
-          >
-            Mi trabajo
-          </Button>
-        </div>
+        <Button
+          variant="ghost"
+          size="lg"
+          render={<Link href="/obras" />}
+          nativeButton={false}
+          className="cursor-pointer text-base py-8 md:text-xl font-bold absolute bottom-4 flex-col animate-float animate-iteration-count-infinite animate-duration-3000"
+        >
+          Mi trabajo
+          <ChevronDown className="size-4" />
+        </Button>
       </div>
     </section>
   );
